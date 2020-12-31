6650008e83c3c4a6f1802c12f0e4271388a18426
Shehraan Rathee <73431328+Shehraan2008@users.noreply.github.com>
2020-12-31 16:58:42 +0530

    Adding Fading
    I have Added a fading system which will make the object fade if they are touched by the the polygon

--- box.js
+++ box.js
@@ -7,18 +7,27 @@ class Box {
     };
     this.body = Bodies.rectangle(x, y, w, h, options);
     World.add(world, this.body);
+    this.visbility = 255;
     this.w = w;
     this.h = h;
   }
   show() {
-    let pos = this.body.position;
-    let angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    strokeWeight(0.3);
-    stroke("red");
-    rect(0, 0, this.w, this.h);
-    pop();
+    if (this.body.speed < 4) {
+      let pos = this.body.position;
+      let angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(0.3);
+      stroke("red");
+      rect(0, 0, this.w, this.h);
+      pop();
+    } else {
+      // World.remove(world, this.body);
+      push();
+      // this.visbility = this.visbility - 15;
+      // tint(255, this.visbility);
+      pop();
+    }
   }
 }
